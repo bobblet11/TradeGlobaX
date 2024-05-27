252cc3d0042707db7cded423bfbc6748260d6465
bobblet11 <benglover78@gmail.com>
2024-05-27 10:27:02 +0800

    used cors anywhere with render.com hosting service instead. Added header crypto news using API call. 1 call every 60 seconds.

--- src/front-end/components/Header/CryptoNewsDiv.js
+++ src/front-end/components/Header/CryptoNewsDiv.js
@@ -1,28 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-export default function CryptoNewsDiv({ value }) {
-    const [info, setInfo] = useState();
-    useEffect(() => {
-        fetch('http://localhost:3000/users',{
-            method:'GET',
-            headers: {
-                'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods' : 'POST, PUT, PATCH, GET, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers' : '*',
-            }
-        }
-        )
-        .then((res) => {
-            console.log(res)
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            setInfo(data);
-        });
-    }, []);
+export default function CryptoNewsDiv({data, value}) {
+    var out = "";
+    if (data[value] === '' || data === undefined){
+        console.log('nothing to update')
+    }else{
+        var symbol = data[value]['symbol'];
+        var price = data[value]['quote']['USD']['price'];
+        out = symbol + ": $" + Math.round(+price, 2) + " USD";
+    }
+
     return (<div className='crypto-news-component'>
-        {info}
+        {out}
     </div>);
 
 }
