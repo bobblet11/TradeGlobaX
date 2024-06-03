c20b4743f24fa3241885998f271f774b3ff7182a
“harrysokool” <harry1232003@icloud.com>
2024-06-03 21:49:17 +0800

    test branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./front-end/components/Header/Header";
 import MainPage from "./front-end/components/MainPage/MainPage";
-import CryptoNewsFooter from "./front-end/components/CryptoNewsFooter/CryptoNewsFooter"
+import CryptoNewsFooter from "./front-end/components/CryptoNewsFooter/CryptoNewsFooter";
+import Footer from "./front-end/components/Footer/Footer";
 
 function App() {
   return (
@@ -11,11 +12,11 @@ function App() {
         <Header />
         <div className="separator-section"></div>
         <MainPage />
-        <CryptoNewsFooter title={"Title"}/>
-        <CryptoNewsFooter title={"Title2"}/>
+        <CryptoNewsFooter title={"Title"} />
+        <CryptoNewsFooter title={"Title2"} />
         <div className="separator-section"></div>
-        <hr/>
-        
+        <Footer />
+        <hr />
       </div>
     </div>
   );
