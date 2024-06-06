e2a0315ad1782161c456a197d19253cb79da2c22
“harrysokool” <harry1232003@icloud.com>
2024-06-06 22:21:14 +0800

    added backbutton

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import BackButton from "../../components/BackButton/BackButton";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <BackButton dest="/" />
+    </div>
+  );
 };
 
 export default LoginPage;
