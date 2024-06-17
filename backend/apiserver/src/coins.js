2f72b26f812a08420a9b2ebf6ebd7b65f132fecd
bobblet11 <benglover78@gmail.com>
2024-06-17 12:21:16 +0800

    reformatedd and improved logging with better try catch blocks.

--- backend/apiserver/src/coins.js
+++ backend/apiserver/src/coins.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import * as DB from "./databaseControls.js"
 import url from 'url'
 import {logToFile} from "./logger.js";
@@ -13,55 +13,143 @@ TradeGlobaX
 Error! 
 
 </div>
+\n
+`;
+
+const successfulInsert = 
+`
+<div>
+
+Successful record insert
+
+</div>
+`;
+
+const databaseError = 
+`
+<div>
+
+Failed DB operation!
+
+</div>
+`;
+
+const badParameters = 
+`
+<div>
+
+Parameters are incorrect
+
+</div>
 `;
 
 
 export const router = express.Router();
 
+function paramsToObject(entries) {
+    const result = {}
+    for(const [key, value] of entries) { // each 'entry' is a [key, value] tupple
+      result[key] = value;
+    }
+    return result;
+  }
+
 router.get('/', async function(req, res){
-    try{
-        logToFile("GET request at /coins/");
+    logToFile("GET request at /coins/");
+    let responseCode = 400;
+    let responseHeader = {'Content-Type': ' text/html'};
+    let responseBody;
+    const queryString = await url.parse(req.url, true).query;
+    const coinToSearch = queryString.coinSymbol;
+    const startStamp = queryString.startStamp;
+    const stopStamp = queryString.stopStamp;
 
-        let responseCode = 400;
-        let responseHeader = {'Content-Type': ' text/html'};
-        let responseBody = failedResponseBody;
+    const startStampDate = new Date(startStamp);
+    const stopStampDate = new Date(stopStamp);
 
-        let queryString = url.parse(req.url, true).query;
-        const coinToSearch = queryString.coinSymbol;
-        const startStamp = queryString.startStamp;
-        const stopStamp = queryString.stopStamp;
-        
-        const startStampDate = new Date(startStamp);
-        const stopStampDate = new Date(stopStamp);
-
-        if ( stopStampDate - startStampDate < 0){
-            logToFile("timestamp Error");
-            res.writeHead(responseCode, responseHeader);
-            res.end(responseBody);
-            return;
-        }
-
-        responseCode = 200;
-        responseHeader = {'Content-Type': 'application/json'}
-        //fix this request params thing, i think this is broken
-        const query = {symbol: coinToSearch, 
-                       timestamp : {$gte : startStamp, $lte : stopStamp}
-        };
-        const projection = {timestamp:1, name:1, symbol:1, price:1};
-
-        responseBody = await DB.query("coins", query, projection);
-        responseBody = await JSON.stringify(responseBody);
-
-        logToFile("writing responseBody");
+    if ( stopStampDate - startStampDate < 0){
+        logToFile("BAD PARAMETER ERROR")
+        logToFile(error);
+        logToFile("------------------------------------------")
+        responseBody = failedResponseBody + badParameters;
         res.writeHead(responseCode, responseHeader);
         res.end(responseBody);
+        return;
     }
-    catch (error){
+
+    const query = {symbol: coinToSearch, 
+                   timestamp : {$gte : startStamp, $lte : stopStamp}
+                  };
+
+    const projection = {timestamp:1, name:1, symbol:1, price:1};
+    try{
+        responseBody = await DB.query("coins", query, projection);
+        responseBody = await JSON.stringify(responseBody);
+    }catch (error){
+        logToFile("DB ERROR")
         logToFile(error);
-    }
-    finally {
-        logToFile("GET request finished")
         logToFile("------------------------------------------")
+        responseBody = failedResponseBody + databaseError;
+        res.writeHead(responseCode, responseHeader);
+        res.end(responseBody);
+        return;
     }
+
+    
+    responseCode = 200;
+    responseHeader = {'Content-Type': 'application/json'}
+    responseBody = successfulInsert;
+    res.writeHead(responseCode, responseHeader);
+    res.end(responseBody);
+
+    logToFile("SUCCESSFUL REQUEST");
+    logToFile("REQUEST DONE");
+    logToFile("------------------------------------------");
+    }
+);
+
+ 
+router.post('/', async function(req, res){
+    logToFile("POST request at /coins/");
+    let responseCode = 400;
+    let responseHeader = {'Content-Type': ' text/html'};
+    let responseBody;
+    let data;
+    const queryString = await url.parse(req.url, true).query;
+
+    try{
+        data = await formatData(queryString);
+    }catch (error){
+        logToFile("BAD PARAMETER ERROR")
+        logToFile(error);
+        logToFile("------------------------------------------")
+        responseBody = failedResponseBody + badParameters;
+        res.writeHead(responseCode, responseHeader);
+        res.end(responseBody);
+        return;
+    }
+
+    try{
+        await DB.writeDB(data);
+    }catch (error){
+        logToFile("DB ERROR")
+        logToFile(error);
+        logToFile("------------------------------------------")
+        responseBody = failedResponseBody + databaseError;
+        res.writeHead(responseCode, responseHeader);
+        res.end(responseBody);
+        return;
+    }
+
+    responseCode = 200;
+    responseHeader = {'Content-Type': 'application/json'}
+    responseBody = successfulInsert;
+    res.writeHead(responseCode, responseHeader);
+    res.end(responseBody);
+
+    logToFile("SUCCESSFUL REQUEST");
+    logToFile("REQUEST DONE");
+    logToFile("------------------------------------------");
  });
+
 
