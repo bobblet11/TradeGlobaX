c6e58556b1fecddd8f6cecd5bf193f1c9d9084c4
bobblet11 <benglover78@gmail.com>
2024-06-17 13:52:45 +0800

    fixed logger

--- backend/apiserver/src/logger.js
+++ backend/apiserver/src/logger.js
@@ -2,11 +2,7 @@ import * as fs from 'fs';
 
 export function logToFile(message) {
     console.log(message);
-    let logStream = fs.createWriteStream('logs.txt',{flags: 'a'})
-    .on('error', function(err){
-        console.log(err.stack);
-     });
-
+    const logStream = fs.createWriteStream("backend/apiserver/src/logs.txt",{flags: 'a'})
     logStream.write(`${message}\n`);
     logStream.end();
 }
--- frontend/src/pages/Frame/Frame.js
+++ frontend/src/pages/Frame/Frame.js
@@ -32,6 +32,7 @@ export default function Frame() {
         console.error(error);
       });
   }
+  
 
   useEffect(() => {
     getCrypto();
